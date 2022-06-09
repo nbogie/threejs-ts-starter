ca2e45dc024f46ddb65bb7b7c85f0d8b44247f4a
nbogie <neillbogie@googlemail.com>
2022-06-09 11:40:06 +0100

    tweak helpers

--- src/setupHelpers.ts
+++ src/setupHelpers.ts
@@ -1,10 +1,10 @@
 import { Scene, AxesHelper, GridHelper } from "three";
 
 export function setupHelpers(scene: Scene): void {
-    const axesHelper = new AxesHelper(10);
-    axesHelper.position.set(-5, 5, 0); //lift up from grid for visibility
+    const axesHelper = new AxesHelper(2);
+    axesHelper.position.set(-8, 6, 0); //lift up from grid for visibility
     scene.add(axesHelper);
     const gridHelper = new GridHelper(100);
-    gridHelper.position.y = 1;
+    // gridHelper.position.y = 1;
     scene.add(gridHelper);
 }
