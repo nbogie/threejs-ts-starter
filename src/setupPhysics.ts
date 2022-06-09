67bc317c2a5645ce70921c8e7585d0ed73af6021
nbogie <neillbogie@googlemail.com>
2022-06-09 13:35:44 +0100

    add fireProjectile

--- src/setupPhysics.ts
+++ src/setupPhysics.ts
@@ -1,22 +1,23 @@
-import * as CANNON from 'cannon-es'
-import { BoxGeometry, Color, DoubleSide, Mesh, MeshStandardMaterial, PlaneGeometry, Quaternion, Scene, Vector3 } from "three";
+import * as CANNON from 'cannon-es';
+import { Body, GSSolver, NaiveBroadphase, World } from 'cannon-es';
+import { BoxGeometry, Camera, Color, DoubleSide, Mesh, MeshStandardMaterial, PlaneGeometry, Quaternion, Scene, Vector3 } from "three";
 import { randFloat, randFloatSpread } from "three/src/math/MathUtils";
 import { pick } from "./randomUtils";
 
-export function setupPhysics(): { world: CANNON.World; } {
-    const world = new CANNON.World();
+export function setupPhysics(): { world: World; } {
+    const world = new World();
     world.gravity.set(0, -9.82, 0); // m/s²
-    world.broadphase = new CANNON.NaiveBroadphase();
-    (world.solver as CANNON.GSSolver).iterations = 10;
+    world.broadphase = new NaiveBroadphase();
+    (world.solver as GSSolver).iterations = 10;
 
     world.defaultContactMaterial.contactEquationStiffness = 1e7;
     world.defaultContactMaterial.contactEquationRelaxation = 4;
     return { world };
 }
 
-export function createGroundBodyAndMesh(world: CANNON.World, scene: Scene): { groundBody: CANNON.Body, groundMesh: Mesh } {
+export function createGroundBodyAndMesh(world: World, scene: Scene): { groundBody: Body, groundMesh: Mesh } {
     const groundShape = new CANNON.Plane()
-    const groundBody = new CANNON.Body({ mass: 0 });
+    const groundBody = new Body({ mass: 0 });
     groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
     groundBody.position.y = 0;
     groundBody.addShape(groundShape);
@@ -28,7 +29,7 @@ export function createGroundBodyAndMesh(world: CANNON.World, scene: Scene): { gr
     return { groundBody, groundMesh };
 }
 
-export function createRandomBoxBodyAndMesh(world: CANNON.World,
+export function createRandomBoxBodyAndMesh(world: World,
     scene: Scene): Mesh {
 
     const body = createRandomBoxBody(world);
@@ -37,11 +38,11 @@ export function createRandomBoxBodyAndMesh(world: CANNON.World,
     return mesh
 }
 
-function createRandomBoxBody(world: CANNON.World): CANNON.Body {
+function createRandomBoxBody(world: World): Body {
 
     const dimensions = new CANNON.Vec3(randFloat(0.3, 0.8), randFloat(0.3, 0.8), randFloat(0.3, 0.8));
 
-    const body = new CANNON.Body({ mass: 30 });
+    const body = new Body({ mass: 30 });
     body.addShape(new CANNON.Box(dimensions));
     body.position.set(randFloatSpread(10), randFloat(5, 15), randFloatSpread(10));
     body.velocity.set(0, 0, 0);
@@ -52,7 +53,7 @@ function createRandomBoxBody(world: CANNON.World): CANNON.Body {
 }
 
 
-function createBoxMeshForBody(body: CANNON.Body, scene: Scene): Mesh {
+function createBoxMeshForBody(body: Body, scene: Scene): Mesh {
     const colourStrings = [
         "#fc354c",
         "#29221f",
@@ -78,9 +79,27 @@ function createBoxMeshForBody(body: CANNON.Body, scene: Scene): Mesh {
 
 
 export function alignMeshToItsBody(mesh: Mesh): void {
-    const body = mesh.userData.body as CANNON.Body;
+    const body = mesh.userData.body as Body;
     console.assert(body, "mesh.userData.body should not be null.  ", mesh)
     mesh.quaternion.copy(body.quaternion as unknown as Quaternion);
     mesh.position.copy(body.position as unknown as Vector3);
 }
 
+export function fireProjectile(world: World, scene: Scene, camera: Camera): Mesh {
+    const camDir = new Vector3();
+    camera.getWorldDirection(camDir);
+    const camPos = camera.position;
+    const spawnOffset = camDir.multiplyScalar(1);//how far in front of cam?  (camDir is normalized.)
+    const spawnPos = new Vector3().addVectors(camPos, spawnOffset);
+
+    const projectileMesh = createRandomBoxBodyAndMesh(world, scene);
+
+    const body = projectileMesh.userData.body as Body;
+
+    body.position.set(spawnPos.x, spawnPos.y, spawnPos.z);
+    projectileMesh.position.copy(spawnPos)
+
+    body.applyImpulse(new CANNON.Vec3(camDir.x, camDir.y, camDir.z).scale(500))
+
+    return projectileMesh;
+}
