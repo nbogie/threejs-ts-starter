fc15a5bcee9d5207ea9bf189617a4a0c03b9d97c
nbogie <neillbogie@googlemail.com>
2022-06-10 14:39:09 +0100

    move grid up off ground plane slightly

--- src/setupHelpers.ts
+++ src/setupHelpers.ts
@@ -5,6 +5,6 @@ export function setupHelpers(scene: Scene): void {
     axesHelper.position.set(-8, 6, 0); //lift up from grid for visibility
     scene.add(axesHelper);
     const gridHelper = new GridHelper(100);
-    // gridHelper.position.y = 1;
+    gridHelper.position.y = 0.1;
     scene.add(gridHelper);
 }
