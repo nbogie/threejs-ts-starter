13ac062b1c195b0238ae4ef5610c28cc51da5f21
nbogie <neillbogie@googlemail.com>
2022-06-12 00:58:10 +0100

    disable camera lerping by default

--- src/setupGUIWithCamera.ts
+++ src/setupGUIWithCamera.ts
@@ -3,10 +3,11 @@ import { Camera } from 'three';
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
 
 export function setupGUIWithCamera(camera: Camera): GUI {
+    camera.userData.desiredPosition = camera.position.clone();
+    camera.userData.shouldLerp = false;
+
     const gui = new GUI();
     const folderCamera = gui.addFolder("camera - desired position");
-    camera.userData.desiredPosition = camera.position.clone();
-    camera.userData.shouldLerp = true;
     folderCamera.add(camera.userData.desiredPosition, "x", -50, 50).listen();
     folderCamera.add(camera.userData.desiredPosition, "y", -50, 50).listen();
     folderCamera.add(camera.userData.desiredPosition, "z", -50, 50).listen();
