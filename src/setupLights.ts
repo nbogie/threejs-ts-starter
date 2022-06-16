79cb850606a772af00aac5a3e0000cee5fe853bc
nbogie <neillbogie@googlemail.com>
2022-06-16 03:15:20 +0100

    provide some dirlight helpers in setupLights

--- src/setupLights.ts
+++ src/setupLights.ts
@@ -1,13 +1,17 @@
-import { AmbientLight, DirectionalLight, Scene } from "three";
+import { AmbientLight, Color, DirectionalLight, DirectionalLightHelper, Scene } from "three";
 
 export function setupLights(scene: Scene): void {
-    const directionalLight1 = new DirectionalLight();
-    directionalLight1.position.set(-2, 3, 2);
+    const directionalLight1 = new DirectionalLight(new Color("white"));
+    directionalLight1.position.set(-10, 15, 10);
     scene.add(directionalLight1);
 
-    const directionalLight2 = new DirectionalLight();
-    directionalLight2.position.set(-5, 2, -2);
+    const directionalLight2 = new DirectionalLight(new Color("blue"));
+    directionalLight2.position.set(-15, 6, -6);
     scene.add(directionalLight2);
+
+    // const helper1 = new DirectionalLightHelper(directionalLight1);
+    // const helper2 = new DirectionalLightHelper(directionalLight2);
+    // scene.add(helper1, helper2);
 
     const ambLight = new AmbientLight(0x604040); // soft white light from everywhere
     scene.add(ambLight);
