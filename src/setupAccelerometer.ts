5497b35071ee3e51fef31d4ae1e03b5df2aaba1a
nbogie <neillbogie@googlemail.com>
2022-06-16 02:28:54 +0100

    placate lint

--- src/setupAccelerometer.ts
+++ src/setupAccelerometer.ts
@@ -1,6 +1,6 @@
 //It seems we need to install @types/w3c-generic-sensor
 
-export function setupAccelerometer(callbackFn: (acl: Accelerometer) => void) {
+export function setupAccelerometer(callbackFn: (acl: Accelerometer) => void): void {
     const acl = new Accelerometer({ frequency: 60 });
     acl.addEventListener('reading', () => callbackFn(acl));
     acl.start();
